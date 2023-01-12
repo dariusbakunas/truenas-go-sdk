330e867a05d65cd703e8a29799d3025d208e3708
Darius Bakunas-Milanowski <bakunas@gmail.com>
2023-01-11 20:08:27 -0500

    refactor: create subfolder for TrueNAS Angelfish release
    With the new structure all generated output will go to specific TrueNAS release subfolder
    
    BREAKING CHANGE: Go module location is changed

--- generate.sh
+++ generate.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-rm -rf ./*.go
-openapi-generator-cli generate -i cfg/openapi.yaml -c cfg/config.yaml -o . -g go --git-user-id dariusbakunas --git-repo-id truenas-go-sdk
-rm -rf test # seems to be broken
-patch client.go < client.patch
+rm -rf angelfish
+openapi-generator-cli generate -i cfg/angelfish.yaml -c cfg/angelfish_config.yaml -o ./angelfish -g go --git-user-id dariusbakunas --git-repo-id truenas-go-sdk
+rm -rf angelfish/test # seems to be broken
+patch angelfish/client.go < client.patch
+mv angelfish/go.mod .
+mv angelfish/go.sum .
+cd angelfish || exit
 go mod tidy
 go fmt ./...
+cd ..
